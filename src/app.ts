e8ee919092ed3febae2ed2c08bc7a93e88620b4f
Manuel Garderes <69734460+manugarderes@users.noreply.github.com>
2026-02-05 18:05:33 -0300

    production

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,8 @@ app.use("/api/account-transactions", accountTransactionsRouter);
 app.use("/api/users", usersRouter);
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 FM CARGO Backend corriendo en puerto ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 FM CARGO Backend corriendo en puerto ${PORT}`);
+  });
+}
